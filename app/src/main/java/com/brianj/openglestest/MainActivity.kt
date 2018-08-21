dfaacc3bf79b855ecfe71e26f3fd17418d8c8fb9
brianmj <spamthiseml@hotmail.com>
2018-08-21 17:34:22 -0400

    Added android lifecycle pause and resume to mainactivity

--- app/src/main/java/com/brianj/openglestest/MainActivity.kt
+++ app/src/main/java/com/brianj/openglestest/MainActivity.kt
@@ -13,9 +13,23 @@ import javax.microedition.khronos.opengles.GL10
 
 class MainActivity : AppCompatActivity(){
 
+    lateinit var surfaceView: MySurfaceView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        surfaceView = myGLView
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        surfaceView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        surfaceView.onPause()
     }
 }
